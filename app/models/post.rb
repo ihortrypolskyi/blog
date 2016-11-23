1ae9dfaf2b1435f2a669b53306acfa4594d8dd4f
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2016-11-23 15:37:51 +0200

    authorization

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100 }
-  validates :body, presence: true, length: { maximum: 500 }
+  validates :body, presence: true, length: { maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+      has_many :posts
+  attr_accessor :remember_token
+  before_save { email.downcase! }
+  validates :first_name, :last_name, :uniqueness => {:case_sensitive => false}, presence: true, length: { in: 2..20 }
+  validates :last_name, :uniqueness => {:case_sensitive => false}, presence: true, length: { in: 2..20 }
   has_secure_password
-  has_many :posts
-  validates :first_name, :last_name, :uniqueness => {:case_sensitive => false}, presence: true, length: { in: 2..10 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
