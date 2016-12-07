4593b1262798d11ee66e7a1cb6d4f3b2098b8fc3
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2016-12-07 23:31:07 +0200

    heroku/admin3

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.setup do |config|
   # == Site Title
   #
-  # Set the title that is displayed on the main layout
+    # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
   config.site_title = "Blog"
