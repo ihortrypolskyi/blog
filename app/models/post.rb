282a2ce672ebffdf77b0eaf0a76841ee605bdad9
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2016-12-13 10:40:07 +0200

    done post image with carrierwave

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  mount_uploader :image, ImageUploader
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100 }, uniqueness: {case_sensitive: false}
   validates :body, presence: true, length: { maximum: 200 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211182847) do
+ActiveRecord::Schema.define(version: 20161213073506) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20161211182847) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
