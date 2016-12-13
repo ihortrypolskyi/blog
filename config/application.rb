3f0d19f5545571a07442ec0d27057afe5af8496c
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2016-12-13 15:20:33 +0200

    before js comments setting

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    #config.time_zone = 'Eastern European Time (UA)'
     config.time_zone = 'Kyiv'
     config.active_record.default_timezone = :local
   end
