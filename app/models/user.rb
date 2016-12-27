2a5eac3b817ce21cca818e310d0abd70790b7bbb
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2016-12-27 13:05:34 +0200

    added jumbotron

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,12 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
-  validates :first_name, :last_name, uniqueness: { case_sensitive: false }, presence: true, length: { in: 2..20 }
-  validates :last_name, uniqueness: { case_sensitive: false }, presence: true, length: { in: 2..20 }
+  validates :first_name, :last_name, uniqueness: { case_sensitive: false }, presence: true, length: { in: 3..30 }
+  validates :last_name, uniqueness: { case_sensitive: false }, presence: true, length: { in: 3..30 }
   has_secure_password
-  validates :password, presence: true, length: { in: 6..20 }, allow_nil: true
+  validates :password, presence: true, length: { in: 3..30 }, allow_nil: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :email, presence: true, length: { in: 3..30}, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   before_save { email.downcase! }
   #castom validation examp: validates_presence_of :body, if: lambda {|post| post.title.length > 10 }
   acts_as_voter
